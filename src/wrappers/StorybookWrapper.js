b8255b4445101a3389e117cf327021d2c45a7ee0
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-06-29 14:37:39 +0530

    FIX module import devtoolbar
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/wrappers/StorybookWrapper.js
+++ src/wrappers/StorybookWrapper.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useReducer } from 'react'
 import PropTypes from 'prop-types'
 import MainStateWrapper from './MainStateWrapper.jsx'
-import DevToolbar from '../components/admin/DashBoard/DevToolbar'
+import DevToolbar from '../components/shared/Admin/Dev/DevToolbar'
 import Roboto from 'typeface-roboto'
 
 const StoryStateWrapper = props => {
