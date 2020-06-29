1c061e08aa9d557077ae1ce35f62001752dc0b40
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-06-29 17:53:55 +0530

    ADD Slide to Designer
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/components/designer/editor/Designer.jsx
+++ src/components/designer/editor/Designer.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
+import Slide from './Slide'
+
 const useStyles = makeStyles(theme => ({
   designer: {
     display: 'flex',
     flexDirection: 'column',
-    backgroundColor: 'red',
     flexGrow: '1',
+    alignItems: 'center',
   },
   viewport: {
     flexGrow: '1',
-    backgroundColor: 'green',
   },
-  slide: {
+  slides: {
+    display: 'flex',
     height: '300px',
-    width: '100%',
-    backgroundColor: 'blue',
+    width: '80%',
+    justifyContent: 'center',
+    overflow: 'scroll',
   },
 }))
 
 const Designer = () => {
   const classes = useStyles()
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   return (
     <div className={classes.designer}>
       <div className={classes.viewport}></div>
-      <div className={classes.slide}></div>
+      <div className={classes.slides}>
+        {data.map(value => (
+          <Slide>value</Slide>
+        ))}
+      </div>
     </div>
   )
 }
