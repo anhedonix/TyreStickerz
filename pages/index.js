e40dcd53bbcb9d0bac027fa5c733918dd19da9bd
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-06-30 12:09:43 +0530

    ADD redefines styles and add background image to link
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,184 +1,64 @@
 import Head from 'next/head'
 import React from 'react'
 import Link from 'next/Link'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
 
 import { name } from '../package.json'
 import MainLayout from '../src/components/shared/Layout/MainLayout'
+// import DesignerLinkBanner from '../public/Banners/DesignerLinkBanner.jpg'
+
+const useStyles = makeStyles((theme) => ({
+  designerLinkCard: {
+    margin: '100px 0 0 0',
+    width: '500px',
+    height: '250px',
+    display: 'flex',
+    backgroundImage: `url(https://http2.mlstatic.com/letras-para-llantas-hechas-de-caucho-D_NQ_NP_725621-MLM20805920899_072016-O.webp)`,
+    backgroundSize: 'cover',
+    backgroundPositionY: '-100px',
+    cursor: 'pointer',
+  },
+  designerLinkCover: {
+    backgroundColor: 'black',
+    width: '100%',
+    display: 'flex',
+    opacity: '.6',
+    '&:hover': {
+      opacity: '.7',
+    },
+  },
+  text: {
+    fontSize: '30px',
+    fontWeight: '500',
+    margin: 'auto',
+  },
+  tittle: {
+    margin: '50px 0 0 0',
+  },
+}))
 
 export default function Home() {
+  const classes = useStyles()
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>{name[0].toUpperCase() + name.substr(1).toLowerCase()}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <MainLayout>
-        <main>
-          <h1 className="title">
-            Welcome to {name[0].toUpperCase() + name.substr(1).toLowerCase()}
-          </h1>
-
-          <Link href='/designer' className='description'>
-            Customize Tyre Stickers
-          </Link>
-        </main>
-
-        <footer>
-          <a
-            href="http://saddlesindia.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Saddles India
-          </a>
-        </footer>
+        <h1 className={classes.tittle}>
+          Welcome to {name[0].toUpperCase() + name.substr(1).toLowerCase()}
+        </h1>
+        <Link href='/designer'>
+          <Paper className={classes.designerLinkCard} elevation='10'>
+            <div className={classes.designerLinkCover}>
+              <div className={classes.text}>Customize Tyre Stickers</div>
+            </div>
+          </Paper>
+        </Link>
       </MainLayout>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
