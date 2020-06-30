b65fca8c7496a54d9e5cc451195160d110764ba6
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-06-30 15:34:49 +0530

    ADD dummy data to DesignerComponent
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/components/designer/editor/Designer.jsx
+++ src/components/designer/editor/Designer.jsx
@@ -2,36 +2,79 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
 import Slide from './Slide'
+import { useEffect } from 'react'
 
 const useStyles = makeStyles(theme => ({
   designer: {
     display: 'flex',
     flexDirection: 'column',
+    width: '100%',
     flexGrow: '1',
     alignItems: 'center',
+    justifyContent: 'space-around',
   },
   viewport: {
     flexGrow: '1',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
   },
   slides: {
     display: 'flex',
     height: '300px',
     width: '80%',
-    justifyContent: 'center',
     overflow: 'scroll',
+  },
+  image: {
+    height: '100%',
+    width: '100',
+    backgroundImage: `url(https://tyrewallstickers.com/wp-content/uploads/2019/08/tyre-2-1.png)`,
+    backgroundSize: '100% 100%',
+  },
+  tyre: {
+    margin: 'auto',
+    // flexGrow: '1',
+    backgroundImage: `url(https://tyrewallstickers.com/wp-content/uploads/2019/08/tyre-2-1.png)`,
+    backgroundSize: '100%',
+    alignSelf: 'center',
+    width: '50vh',
+    height: '50vh',
+    backgroundPositionY: '-20px',
   },
 }))
 
 const Designer = () => {
   const classes = useStyles()
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const data = {
+    image: <div className={classes.image} />,
+    info: {
+      header: 'Header',
+      description: 'description',
+      meta1: 'meta1',
+      meta2: 'meta2',
+    },
+  }
+
+  let list = []
+
+  const addItems = () => {
+    let i
+    for (i = 0; i < 100; i++) {
+      list.push(data)
+    }
+    return list
+  }
+  addItems()
 
   return (
     <div className={classes.designer}>
-      <div className={classes.viewport}></div>
+      <div className={classes.viewport}>
+        <div className={classes.tyre} />
+      </div>
+
       <div className={classes.slides}>
-        {data.map(value => (
-          <Slide>value</Slide>
+        {list.map(value => (
+          <Slide info={value.info} image={value.image} />
         ))}
       </div>
     </div>
