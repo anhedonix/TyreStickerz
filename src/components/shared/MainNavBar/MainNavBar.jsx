5005e42d1027d86c22e0cf7ff7e8ee3c96abaae4
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-07-03 17:35:06 +0530

    FIX imports 'Link'
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/components/shared/MainNavBar/MainNavBar.jsx
+++ src/components/shared/MainNavBar/MainNavBar.jsx
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
-import Link from 'next/link'
+import Link from 'next/Link'
 import React from 'react'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
