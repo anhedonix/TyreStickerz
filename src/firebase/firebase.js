c3f84004c2b301010ae7cfd7a9160c48fc708b53
Anand Magaji <anhedonix@gmail.com>
2020-07-09 13:40:44 +0530

    DROP Analytics
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,13 +7,18 @@ import 'firebase/firestore'
 import 'firebase/storage'
 import 'firebase/performance'
 
+// let analytics
+
 if (!app.apps.length) {
   app.initializeApp(config)
+  // if (typeof window !== 'undefined') {
+  // if ('measurementId' in clientCredentials) analytics = app.analytics()
+  // }
 }
 
 export default app.app()
 
-export const analytics = app.analytics()
+// export { analytics }
 export const auth = app.auth()
 export const firestore = app.firestore()
 export const storage = app.storage()
