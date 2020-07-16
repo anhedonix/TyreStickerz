0982b0c36185c6239c5e8591f00a2e8408a9831d
Anand Magaji <anhedonix@gmail.com>
2020-07-16 18:42:26 +0530

    ADD Content Menu
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/components/dashboard/ContentMenu.jsx
+++ src/components/dashboard/ContentMenu.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import AdminContentTypes from '../../constants/admin'
+import Link from 'next/Link'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import DashboardIcon from '@material-ui/icons/Dashboard'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -8,24 +15,38 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     borderRight: '1px solid rgba(128,128,128,0.5)',
   },
-  menuItem: {
-    padding: '1rem 2rem',
-  },
 }))
 
 const MenuItem = props => {
-  const classes = useStyles()
-  return <div className={classes.menuItem}>{props.label}</div>
+  return (
+    <Link href={`/dashboard/[contentType]`} as={`/dashboard/${props.ID}`}>
+      <ListItem button>
+        <ListItemIcon>{props.extra.icon}</ListItemIcon>
+        <ListItemText primary={props.label} />
+      </ListItem>
+    </Link>
+  )
 }
 
-const ContentMenu = props => {
+const ContentMenu = () => {
   const classes = useStyles()
   return (
-    <div className={classes.root}>
+    <List className={classes.root}>
+      <ListItem button>
+        <Link href={`/dashboard`} as={`/dashboard`}>
+          <a className={classes.anchor}>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </a>
+        </Link>
+      </ListItem>
+      <Divider />
       {AdminContentTypes.map(el => (
         <MenuItem {...el} key={el.ID} />
       ))}
-    </div>
+    </List>
   )
 }
 
