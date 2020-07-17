c9382ba35b8aa319da5d1a9d4717acd5c7e8570d
Anand Magaji <anhedonix@gmail.com>
2020-07-17 19:31:28 +0530

    ADD ContentType & ContentId to router parms
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- pages/dashboard/[contentType]/[contentId].js
+++ pages/dashboard/[contentType]/[contentId].js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import React from 'react'
 
-import { name } from '../../package.json'
-import MainLayout from '../../src/components/shared/Layout/MainLayout'
-import Designer from '../../src/components/designer/editor'
-import DashboardLayout from '../../src/components/dashboard/DashboardLayout'
+import { name } from '../../../package.json'
+import MainLayout from '../../../src/components/shared/Layout/MainLayout'
+import Designer from '../../../src/components/designer/editor'
+import DashboardLayout from '../../../src/components/dashboard/DashboardLayout'
 
 export default function designer() {
   return (
