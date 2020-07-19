832f7da970aac23c02ed5aad4c88d4791462d1a9
Anand Magaji <anhedonix@gmail.com>
2020-07-19 15:17:50 +0530

    ADD Individual Data handling in fields
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/components/dashboard/Content/ContentFieldView.jsx
+++ src/components/dashboard/Content/ContentFieldView.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import moment from 'moment'
 
 const ContentFieldView = props => {
   const { id, label, type, data } = props
-  console.log(data)
   return (
     <TableRow>
       <TableCell align="right">{label}</TableCell>
       <TableCell>
-        {type === 'string'
-          ? data[id]
-          : type === 'uid'
-          ? data[id]
+        {['string', 'int', 'uid'].includes(type)
+          ? data
           : type === 'timestamp'
-          ? moment(data.timestamp.toDate()).format('YYYY MM DD LT')
+          ? moment(data.toDate()).format('YYYY MM DD LT')
+          : type === 'bool'
+          ? data
+            ? 'true'
+            : 'false'
           : null}
       </TableCell>
     </TableRow>
--- src/components/dashboard/Content/ContentViewer.jsx
+++ src/components/dashboard/Content/ContentViewer.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
 
 import * as CONTENT from '../../../constants/contentTypes'
 import ContentFieldView from './ContentFieldView'
@@ -18,7 +14,7 @@ const ContentViewer = props => {
         {contentType &&
           contentId &&
           CONTENT[contentType].fields.map(el => (
-            <ContentFieldView data={data} {...el} key={el.id} />
+            <ContentFieldView data={data[el.id]} {...el} key={el.id} />
           ))}
       </TableBody>
     </Table>
