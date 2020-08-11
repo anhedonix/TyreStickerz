4d713f2be087dc0cd93d13065cd818ff1fcfb107
Anand Magaji <anhedonix@gmail.com>
2020-08-11 18:47:15 +0530

    ADD Delete confirmation dialogue to individual contents.
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/components/dashboard/Content/index.js
+++ src/components/dashboard/Content/index.js
@@ -4,6 +4,11 @@ import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
 
 import ContentEditor from './ContentEditor'
 import ContentViewer from './ContentViewer'
@@ -34,6 +39,8 @@ const Content = () => {
   const [data, setData] = useState()
   const [cData, setCData] = useState()
   const [loading, setLoading] = useState(true)
+
+  const [confirmDelete, setConfirmDelete] = useState(false)
 
   const router = useRouter()
   const { contentType, contentId } = router.query
@@ -123,18 +130,52 @@ const Content = () => {
           )}
           {CONTENT[contentType].extra.adminActions.includes('delete') &&
             contentId !== 'create' && (
+              <Dialog
+                open={confirmDelete}
+                onClose={() => setConfirmDelete(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {'Are you sure you want to delete ' + contentType + '?'}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    This action cannot be undone, the data will be permenantaly
+                    deleted, are you sure you want to continue?
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => setConfirmDelete(false)}>
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setConfirmDelete(false)
+                      setEditMode(false)
+                      setIsEdited(false)
+                      CONTENT[contentType].delete(contentId).then(() => {
+                        router.push(
+                          '/dashboard/[contentType]',
+                          `/dashboard/${contentType}`
+                        )
+                      })
+                    }}
+                    color="primary"
+                    autoFocus
+                  >
+                    Delete
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            )}
+          {CONTENT[contentType].extra.adminActions.includes('delete') &&
+            contentId !== 'create' && (
               <Button
                 className={classes.editButton}
                 variant="outlined"
                 onClick={() => {
-                  setEditMode(false)
-                  setIsEdited(false)
-                  CONTENT[contentType].delete(contentId).then(() => {
-                    router.push(
-                      '/dashboard/[contentType]',
-                      `/dashboard/${contentType}`
-                    )
-                  })
+                  setConfirmDelete(true)
                 }}
               >
                 Delete
