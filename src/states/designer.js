9e6360e5300c075599bc6784769ad89e6e2b93ea
Anand Magaji <anhedonix@gmail.com>
2020-08-14 20:22:11 +0530

    ADD Default Env Paths from public folder
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/states/designer.js
+++ src/states/designer.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import _ from 'lodash'
+import store from '../functions/store'
 
 export const initialState = {
   canvas: null,
   geo: [],
+  env: {
+    px: '/resources/cubemaps/px.hdr',
+    nx: '/resources/cubemaps/nx.hdr',
+    py: '/resources/cubemaps/py.hdr',
+    ny: '/resources/cubemaps/ny.hdr',
+    pz: '/resources/cubemaps/pz.hdr',
+    nz: '/resources/cubemaps/nz.hdr',
+  },
 }
 
 export const DesignerContext = React.createContext({
@@ -21,6 +30,15 @@ export const reducer = (state, action) => {
 
     case 'defaults': {
       return { ...state, defaults: action.payload }
+    }
+
+    case 'setEnv': {
+      const maps = ['px', 'nx', 'py', 'ny', 'pz', 'nz']
+      const out = {}
+      maps.map(el =>
+        store.getFileUrl(action.payload[el]).then(p => (out[el] = p))
+      )
+      return { ...state, env: out }
     }
 
     default: {
