7e96b1a7b7a773dc4ff202a18993fb6c9211d889
Anand Magaji <anhedonix@gmail.com>
2020-08-14 20:43:30 +0530

    ADD default Canvas
    Co-authored-by: Anand Magaji <anand@eosacro.com>
    
    Detail: canvas with camera framing the objects. OrbitControls with default
    target, minDistance, maxDistance. Pan is disabled. Default Shadow object. Load
    all objects from the default state.

--- src/components/designer/editor/Canvas/index.js
+++ src/components/designer/editor/Canvas/index.js
@@ -1,60 +1,69 @@
-import React, { useRef, useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState, useContext, Suspense } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 import axios from 'axios'
 import GLBLoader from './Loader/GLBLoader'
-import { OrbitControls } from 'drei'
+import { OrbitControls, Shadow } from 'drei'
 
 import * as CONTENT from '../../../../constants/contentTypes'
 
 import { DesignerContext } from '../../../../states/designer'
-
-function Box(props) {
-  const mesh = useRef()
-
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={e => setActive(!active)}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach="material"
-        color={hovered ? 'hotpink' : 'orange'}
-      />
-    </mesh>
-  )
-}
+import Env from './Loader/Env'
 
 const MainCanvas = props => {
-  const { state } = useContext(DesignerContext)
+  const { state, dispatch } = useContext(DesignerContext)
   const [model, setModel] = useState()
   const [modelPath, setModelPath] = useState('')
 
+  const [cluster, setCluster] = useState('')
+  const [clusterpath, setClusterpath] = useState([])
+
   useEffect(() => {
-    axios.get('/api/defaults').then(i => setModel(i.data.whl))
+    axios.get('/api/defaults').then(i => {
+      setCluster(i.data.rim)
+      setModel(i.data.whl)
+    })
+    axios
+      .get('/api/defaults/env')
+      .then(i => dispatch({ type: 'setEnv', payload: i.data }))
   }, [])
 
   useEffect(() => {
     CONTENT.wheel.read(model).then(i => setModelPath(i.tyre))
   }, [model])
 
+  useEffect(() => {
+    CONTENT.rims.read(cluster).then(i => {
+      const cluster_tmp = []
+      if (i.model) {
+        Object.keys(i.model).map(key => {
+          cluster_tmp.push(i.model[key].model)
+        })
+      }
+      setClusterpath(cluster_tmp)
+    })
+  }, [cluster])
+
   return (
-    <Canvas ref={MainCanvas}>
+    <Canvas camera={{ position: [80, 60, -100], fov: 30 }}>
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls />
-      {modelPath && <GLBLoader path={modelPath} />}
+      {/* <pointLight position={[100, 100, 100]} /> */}
+      <Env env={state.env} />
+      <Suspense fallback={null}>
+        {modelPath && <GLBLoader path={modelPath} />}
+        {clusterpath && clusterpath.map(p => <GLBLoader path={p} key={p} />)}
+      </Suspense>
+      <Shadow
+        scale={[20, 20, 1]}
+        opacity={0.8}
+        position={[0, 0, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      />
+      <OrbitControls
+        target={[0, 30, 0]}
+        minDistance={120}
+        maxDistance={180}
+        enablePan={false}
+      />
     </Canvas>
   )
 }
