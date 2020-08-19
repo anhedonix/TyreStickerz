c2db2fedfcf94b9c9af7769dbf4f1f4d9809486a
hemanthkumarkh <kh.hemanthkumar@gmail.com>
2020-08-19 16:22:28 +0530

    ADD temporary designer component
    Co-authored-by: Hemanth Kumar <kh.hemanthkumar@gmail.com>

--- src/components/designer/editor/Designer_1.jsx
+++ src/components/designer/editor/Designer_1.jsx
@@ -1,3 +1,36 @@
-import Canvas from './Canvas/index'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default Canvas
+import Canvas from './Canvas/index'
+import StickerEditor from './StickerEditor/StickerEditor'
+
+const useStyles = makeStyles(theme => ({
+  designer: {
+    display: 'flex',
+    width: '100vw',
+    height: `calc(100vh - 100px)`,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundImage: `url('/BGs/DesignerBanner.png')`,
+    backgroundSize: '100% 100%',
+  },
+  canvas: {
+    width: '80vh',
+    height: `calc(80vh - 100px)`,
+  },
+}))
+
+const Designer = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.designer}>
+      <div className={classes.canvas}>
+        <Canvas />
+      </div>
+      <StickerEditor />
+    </div>
+  )
+}
+
+export default Designer
