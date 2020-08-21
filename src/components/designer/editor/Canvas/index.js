9ea8bf3b0332c50afe2e1de2f94d9621da0fa9bb
Anand Magaji <anhedonix@gmail.com>
2020-08-21 23:50:33 +0530

    ADD Scene environment initialization options
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/components/designer/editor/Canvas/index.js
+++ src/components/designer/editor/Canvas/index.js
@@ -1,17 +1,13 @@
-import React, { useEffect, useState, useContext, Suspense } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
 import axios from 'axios'
-import GLBLoader from './Loader/GLBLoader'
 import { OrbitControls, Shadow } from 'drei'
-
+import React, { useEffect, useState } from 'react'
+import { Canvas } from 'react-three-fiber'
 import * as CONTENT from '../../../../constants/contentTypes'
-
-import { DesignerContext } from '../../../../states/designer'
 import Env from './Loader/Env'
+import GLBLoader from './Loader/GLBLoader'
 
-const MainCanvas = props => {
-  const { state, dispatch } = useContext(DesignerContext)
-
+const MainCanvas = () => {
+  const [init, setInit] = useState(false)
   const [wheel, setWheel] = useState()
   const [rim, setRim] = useState()
   const [accessories, setAccessories] = useState()
@@ -27,17 +23,18 @@ const MainCanvas = props => {
   return (
     <Canvas camera={{ position: [80, 60, -100], fov: 30 }}>
       <ambientLight intensity={0.3} />
-      {/* <pointLight position={[100, 100, 100]} /> */}
-      <Env />
-      {rim && <GLBLoader path={rim} />}
-      {wheel && <GLBLoader path={wheel} />}
-      {accessories && <GLBLoader path={accessories} />}
-      <Shadow
-        scale={[20, 20, 1]}
-        opacity={0.8}
-        position={[0, 0, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      />
+      <Env initialize={() => setInit(true)} />
+      {init && rim && <GLBLoader path={rim} />}
+      {init && wheel && <GLBLoader path={wheel} />}
+      {init && accessories && <GLBLoader path={accessories} />}
+      {init && (
+        <Shadow
+          scale={[20, 20, 1]}
+          opacity={0.8}
+          position={[0, 0, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+        />
+      )}
       <OrbitControls
         target={[0, 30, 0]}
         minDistance={120}
