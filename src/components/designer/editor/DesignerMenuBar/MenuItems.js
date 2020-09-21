930af5628df36277d5ea20079783e022d469de82
Anand Magaji <anhedonix@gmail.com>
2020-09-21 15:13:09 +0530

    DROP functions
    Co-authored-by: Anand Magaji <anand@eosacro.com>

--- src/components/designer/editor/DesignerMenuBar/MenuItems.js
+++ src/components/designer/editor/DesignerMenuBar/MenuItems.js
@@ -5,34 +5,3 @@ const file = {
 const help = { head: 'help', list: ['About', 'Help'] }
 
 export const items = [file, help]
-
-export const listItemFunctions = (listItem, action) => {
-  switch (listItem) {
-    case 'New':
-      console.log('new')
-      break
-    case 'Open':
-      console.log('open')
-      break
-    case 'Save':
-      console.log('save')
-      break
-    case 'Reset':
-      console.log('reset')
-      break
-    case 'Save Product Image':
-      console.log('save product image')
-      break
-    case 'About':
-      action(true)
-      console.log('about')
-      break
-    case 'Help':
-      console.log('help')
-      break
-    default:
-      break
-  }
-}
-
-// export default { file, help }
